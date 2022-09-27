b0be1c2345ef46416c6f46ee377e3b18db3b4975
1-MuhammadAli <ali.oraxtech@gmail.com>
2022-09-27 16:10:10 +0500

    add walkthroughs screen

--- lib/walkthroughs_screen.dart
+++ lib/walkthroughs_screen.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
-class WalkthroughScreen extends StatelessWidget {
-  const WalkthroughScreen({Key? key}) : super(key: key);
+class WalkthroughScreen extends StatefulWidget {
+   const WalkthroughScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WalkthroughScreen> createState() => _WalkthroughScreenState();
+}
+
+class _WalkthroughScreenState extends State<WalkthroughScreen> {
+  int activeIndex = 0;
+
+  List listStock = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Assignment#02'),
+      body: Stack(
+        children: [
+          PageView.builder(
+              onPageChanged: (index) {
+                setState(() {
+                  activeIndex = index;
+                });
+              },
+              scrollDirection: Axis.horizontal,
+              controller: PageController(viewportFraction: 1),
+              itemCount: listStock.length,
+              itemBuilder: (context, index) {
+                return listStock[index];
+              }),
+          Container(
+            alignment: Alignment.bottomCenter,
+            height: 150,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ...List.generate(
+                  listStock.length,
+                      (index) => Indicator(
+                    isActive: activeIndex == index ? true : false,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(child: Text('Hello')),
     );
   }
 }
