521fb426da7b51d4a5e21dcb4fd959ec7f5f0e45
1-MuhammadAli <ali.oraxtech@gmail.com>
2022-09-27 18:55:30 +0500

    add walkthrough features

---
+++ lib/walkthrough_page2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WalkthroughPage2 extends StatelessWidget {
+  const WalkthroughPage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
--- lib/walkthroughs_screen.dart
+++ lib/walkthroughs_screen.dart
@@ -1,4 +1,8 @@
+import 'package:assignment_no_2/walkthrough_page1.dart';
+import 'package:assignment_no_2/walkthrough_page2.dart';
+import 'package:assignment_no_2/walkthrough_page3.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WalkthroughScreen extends StatefulWidget {
    const WalkthroughScreen({Key? key}) : super(key: key);
@@ -11,44 +15,107 @@ class _WalkthroughScreenState extends State<WalkthroughScreen> {
   int activeIndex = 0;
 
   List listStock = [
-    Page1(),
-    Page2(),
-    Page3(),
+    const WalkthroughPage1(),
+    const WalkthroughPage2(),
+    const WalkthroughPage3(),
+
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    var screenSize=MediaQuery.of(context).size;
+    return Container(
+      height: screenSize.height*1,
+      child: Column(
         children: [
-          PageView.builder(
-              onPageChanged: (index) {
-                setState(() {
-                  activeIndex = index;
-                });
-              },
-              scrollDirection: Axis.horizontal,
-              controller: PageController(viewportFraction: 1),
-              itemCount: listStock.length,
-              itemBuilder: (context, index) {
-                return listStock[index];
-              }),
           Container(
-            alignment: Alignment.bottomCenter,
-            height: 150,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ...List.generate(
-                  listStock.length,
-                      (index) => Indicator(
-                    isActive: activeIndex == index ? true : false,
+            height: screenSize.height*0.8,
+            child: PageView.builder(
+                    onPageChanged: (index) {
+                      setState(() {
+                        activeIndex = index;
+                      });
+                    },
+                    scrollDirection: Axis.horizontal,
+                    controller: PageController(viewportFraction: 1),
+                    itemCount: listStock.length,
+                    itemBuilder: (context, index) {
+                      return listStock[index];
+                    }),
+          ),
+          Column(
+            children: [
+              Container(
+                height: screenSize.height*0.1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ...List.generate(
+                      listStock.length,
+                          (index) => Indicator(
+                        isActive: activeIndex == index ? true : false,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: 218,
+                height: 48,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: [0.0, 1.0],
+                    colors: [
+                      Colors.pinkAccent,
+                      Colors.deepPurple,
+                    ],
                   ),
-                )
-              ],
-            ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: Text('Get Started',
+                      textAlign: TextAlign.center,
+                  style: GoogleFonts.raleway(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16
+                    )
+                  )
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    // elevation: MaterialStateProperty.all(3),
+                    //shadowColor: Colors.transparent,
+                  ),
+                ),
+              )
+            ],
           ),
         ],
+      ),
+
+    );
+  }
+}
+class Indicator extends StatelessWidget {
+  final bool isActive;
+  const Indicator({
+    Key? key,
+    required this.isActive,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 3.0),
+      height: 8.0,
+      width: isActive ? 8.0 : 8.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: isActive ? Colors.green : Colors.white,
       ),
     );
   }
