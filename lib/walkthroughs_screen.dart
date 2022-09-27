1c9ebf16ae9c7cdf735060713e51c5005f10679b
1-MuhammadAli <ali.oraxtech@gmail.com>
2022-09-27 15:41:04 +0500

    Initial commit

---
+++ lib/walkthroughs_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WalkthroughScreen extends StatelessWidget {
+  const WalkthroughScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assignment#02'),
+      ),
+      body: Center(child: Text('Hello')),
+    );
+  }
+}
